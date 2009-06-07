408bd6be0ab22bc1ab4d1794fc29ab6f7e838d41
Frank Mullenger <frank_mullenger@yahoo.com>
2009-06-07 16:24:12 +0100

    fb app

--- actions/tellcupidhow.php
+++ actions/tellcupidhow.php
@@ -41,6 +41,8 @@ class TellCupidHowAction extends FacebookAction
 
     function showContent()
     {
+        $this->elementStart('div', array('id' => 'content', 'class' => 'hentry'));
+        $this->elementStart('div', array('id' => 'content_inner', 'class' => 'entry-content'));
         $this->element('h2', null, _('How Does This Work?'));
         $content = <<<EOS
 <p>
@@ -75,6 +77,8 @@ and respond to other users.
 </p>
 EOS;
         $this->raw($content);
+        $this->elementEnd('div');
+        $this->elementEnd('div');
     }
     
     function title() 
